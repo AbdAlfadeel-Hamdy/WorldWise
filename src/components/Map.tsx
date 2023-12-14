cc02ac099007e08e79a778bf36d2ded320ae96b5
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-12-14 09:20:32 +0200

    Add leaflet map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,11 +1,38 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { LatLngTuple } from 'leaflet';
+import { useCities } from '../contexts';
 import styles from './Map.module.css';
 
 const Map = () => {
   const navigate = useNavigate();
+  const { cities } = useCities();
+  console.log(cities);
+  const [mapPosition, setMapPosition] = useState([
+    51.505, -0.09,
+  ] as LatLngTuple);
+
+  const renderedMarkers = cities.map((city) => (
+    <Marker key={city.id} position={[city.position.lat, city.position.lng]}>
+      <Popup>{city.notes}</Popup>
+    </Marker>
+  ));
+
   return (
     <div className={styles.mapContainer} onClick={() => navigate('form')}>
-      Map
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+        />
+        {renderedMarkers}
+      </MapContainer>
     </div>
   );
 };
